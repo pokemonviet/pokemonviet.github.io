955890985064683035f1dd81c3042e83291a6265
nguyen_minh_nhut <minhnhut.devcntt@gmail.com>
2023-03-03 23:38:03 +0700

    add

---
+++ pokemon-site/src/components/commons/Background.vue
@@ -0,0 +1,28 @@
+<template>
+    <section id="hero" class="hero d-flex align-items-center">
+
+<div class="container">
+  <div class="row">
+    <div class="col-3 hero-img" data-aos="zoom-out" data-aos-delay="200">
+      <img src="https://lh6.googleusercontent.com/hC4NZrsrlSy1aZJpaBsNBevjVdvJQhm1sM5UmkiVkoqM74JmzXiffht1ZmdQRtdGm9c3worEdRxwVo2X1HDbuQ4" class="img-fluid" alt="">
+    </div>
+    <div class="col-sm-6 d-flex flex-column justify-content-center">
+      <h1 class="text-center" data-aos="fade-up">Paradox Pokemon mới xuất hiện <br><span style="color:green">Iron Leaves</span> & <span style="color:blue">Waking Wake</span></h1>
+      <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
+      <div data-aos="fade-up" data-aos-delay="600">
+        <div class="text-center text-lg-start">
+          <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+            <span>Get Started</span>
+            <i class="bi bi-arrow-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="col-3 hero-img" data-aos="zoom-out" data-aos-delay="200">
+      <img src="https://lh3.googleusercontent.com/6Y7zUkvKim3K4OWqt9oRjgIQIEsVBJ4bnp33EXE8RB0iaT9UWEHxvr2YHor-am8vJRmbV_VGJC4Vu5rlqT7C-OU" class="img-fluid" alt="">
+    </div>
+  </div>
+</div>
+
+</section><!-- End Hero -->
+</template>
---
+++ pokemon-site/src/components/commons/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+    <a href="index.html" class="logo d-flex align-items-center">
+        <img src="/src/assets/img/pikachu.png" alt="">
+        <span>Pokemon Viet</span>
+    </a>
+</template>
