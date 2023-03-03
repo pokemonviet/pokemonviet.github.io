3a7b41559b46750b034dbb27e32064af4d1e7604
[Lampart] Nguyen Minh Nhut <nguyen_minh_nhut@lampart-vn.com>
2023-03-03 16:35:31 +0700

    commit source vue3

---
+++ vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/commons/Header.vue'
+import BackgroundVue from './components/commons/Background.vue'
+import Footer from './components/commons/Footer.vue'
+</script>
+
+<template>
+  <header>
+    <Header></Header>
+    <BackgroundVue></BackgroundVue>
+    
+    <Footer></Footer>
+  </header>
+</template>
+
