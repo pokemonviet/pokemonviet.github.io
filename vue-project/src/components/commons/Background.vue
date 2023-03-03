ae8ffd732ba0a73dfaacb5723e26cc95ebb2ba8d
[Lampart] Nguyen Minh Nhut <nguyen_minh_nhut@lampart-vn.com>
2023-03-03 16:52:46 +0700

    push source

--- vue-project/src/components/commons/Background.vue
+++ vue-project/src/components/commons/Background.vue
@@ -3,8 +3,11 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-lg-6 d-flex flex-column justify-content-center">
-      <h1 data-aos="fade-up">We offer modern solutions for growing your business</h1>
+    <div class="col-3 hero-img" data-aos="zoom-out" data-aos-delay="200">
+      <img src="https://lh6.googleusercontent.com/hC4NZrsrlSy1aZJpaBsNBevjVdvJQhm1sM5UmkiVkoqM74JmzXiffht1ZmdQRtdGm9c3worEdRxwVo2X1HDbuQ4" class="img-fluid" alt="">
+    </div>
+    <div class="col-sm-6 d-flex flex-column justify-content-center">
+      <h1 class="text-center" data-aos="fade-up">Paradox Pokemon mới xuất hiện <br><span style="color:green">Iron Leaves</span> & <span style="color:blue">Waking Wake</span></h1>
       <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
       <div data-aos="fade-up" data-aos-delay="600">
         <div class="text-center text-lg-start">
@@ -15,8 +18,8 @@
         </div>
       </div>
     </div>
-    <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
-      <img src="/src/assets/img/hero-img.png" class="img-fluid" alt="">
+    <div class="col-3 hero-img" data-aos="zoom-out" data-aos-delay="200">
+      <img src="https://lh3.googleusercontent.com/6Y7zUkvKim3K4OWqt9oRjgIQIEsVBJ4bnp33EXE8RB0iaT9UWEHxvr2YHor-am8vJRmbV_VGJC4Vu5rlqT7C-OU" class="img-fluid" alt="">
     </div>
   </div>
 </div>
