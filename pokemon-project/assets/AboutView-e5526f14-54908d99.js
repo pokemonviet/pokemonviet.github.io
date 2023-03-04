7ce09ebc23a7aa13517e0936ec5d8b59c8fca702
nguyen_minh_nhut <minhnhut.devcntt@gmail.com>
2023-03-04 20:49:35 +0700

    Update Code 8:49 04/03/2023

---
+++ pokemon-project/assets/AboutView-e5526f14-54908d99.js
@@ -0,0 +1,1 @@
+import{N as a,V as s,f as n,a as t}from"./index-8a508db2.js";const o={},r={class:"about"},e=a("h1",null,"This is an about page",-1),u=[e];function c(i,f){return n(),t("div",r,u)}const d=s(o,[["render",c]]);export{d as default};
